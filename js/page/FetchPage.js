c3437ae631c50c76faba4e845713567a4703d74c
mac <mac@qq.com>
2018-12-31 13:24:59 +0800

    13:24

--- js/page/FetchPage.js
+++ js/page/FetchPage.js
@@ -12,9 +12,18 @@ export default class FetchPage extends Component {
     loadData() {
         // https://api.github.com/search/repositories?q=java
         let url = `https://api.github.com/search/repositories?q=${this.searchKey}`;
-        fetch(url).then(response => response.text()).then(responseText => {
+        fetch(url).then(response => {
+            if (response.ok) {
+                return response.text();
+            }
+            throw new Error('Network response was not ok.');
+        }).then(responseText => {
             this.setState({
                 showText: responseText
+            })
+        }).catch(e => {
+            this.setState({
+                showText: e.toString()
             })
         })
     }
